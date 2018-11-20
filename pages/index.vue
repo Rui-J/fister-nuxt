91296d7b4f439dcd2b751c4d81a8c1acea75ed29
jiangrui <jiangrui@do1.com.cn>
2018-11-20 21:53:47 +0800

    first

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <header></header>
+    <section></section>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+</style>
